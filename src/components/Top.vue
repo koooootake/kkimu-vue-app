98ab3b03c7a51d5f3c6a81d889161ea559bc035a
kkimu <kimura.kzm@gmail.com>
2018-06-21 23:56:49 +0900

    Update Top component

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -1,16 +1,30 @@
 <template>
   <div class="Top">
-    <h1>{{ msg }}</h1>
-    <h2>descripdescriptiondescriptiondescriptiondescription</h2>
+    <h1>{{ title }}</h1>
+    <h2>About</h2>
+    <img src="../assets/icon.png" height="150" >
+    <h3>Profile</h3>
+    <p>木邑 和馬 / Kazuma Kimura</p>
+    <p>株式会社サイバーエージェントで働くサーバサイドエンジニア</p>
+    <h3>Social</h3>
+    <ul>
+      <li>
+        <a href="https://twitter.com/_kkimu" target="_blank">Twitter</a>
+      </li>
+      <li>
+        <a href="https://github.com/kkimu" target="_blank">Github</a>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Top',
-  data () {
+  data: () => {
     return {
-      msg: 'kkimu\'s page'
+      title: 'kkimu',
+      description: 'kkimuaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa',
     }
   }
 }
@@ -18,16 +32,29 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h1, h2, h3 {
   font-weight: normal;
+  word-wrap: break-word;
+}
+h1 {
+  font-size: 4rem;
+}
+h2 {
+  font-size: 3rem;
+}
+h3 {
+  font-size: 2rem;
+}
+img {
+  height:
 }
 ul {
   list-style-type: none;
   padding: 0;
 }
 li {
-  display: inline-block;
   margin: 0 10px;
+  line-height: 2;
 }
 a {
   color: #42b983;
