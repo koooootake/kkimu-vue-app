d89d76de4bb57ea348872458855bc4e772e7328e
kkimu <kimura.kzm@gmail.com>
2018-06-24 23:03:10 +0900

    Add embedded youtube

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,6 +1,9 @@
 <template>
   <div class="Product">
     <p>{{title}}</p>
+    <div v-if="youtube" class="iframe-content">
+      <iframe v-bind:src="youtube" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
+    </div>
     <p class="lead">{{description}}</p>
   </div>
 </template>
@@ -8,11 +11,14 @@
 <script>
 export default {
   name: 'Product',
-  props: ['title', 'description'],
+  props: ['title', 'description', 'youtube', 'niconico'],
 };
 </script>
 
 <style scoped>
+.Product {
+  padding-bottom: 10px;
+}
 p {
   font-family: 'Ruda', sans-serif;
   font-size: 16px;
@@ -24,5 +30,18 @@ p.lead {
   font-size: 14px;
   font-weight: 400;
   margin-top: -15px;
+}
+.iframe-content {
+  position: relative;
+  width: 100%;
+  height: 0;
+  padding-top: 75%;
+}
+.iframe-content iframe {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
 }
 </style>
